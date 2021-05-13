c91120fcef3cc02515b9133319672159a45451a2
Michael Mergell <michael.mergell@microsoft.com>
2021-05-13 19:16:50 +0200

    update

--- Scripts/BackupDisable.ps1
+++ Scripts/BackupDisable.ps1
@@ -97,8 +97,10 @@ Write-Host $CONTDIS1
     }
     else {
         Write-Host "-------Container found, backup will be removed-------"
-        Write-Host "az backup protection disable -c "$CONTAINER1" -g $RGV -v $RSV --item-name $VM"
+        Write-Host "az backup protection disable -c '$CONTAINER1' -g $RGV -v $RSV --item-name $VM"
         az backup protection disable -c "$CONTAINER1" -g $RGV -v $RSV --item-name $VM --yes
+        Write-Host "az backup container unregister -c '$CONTAINER1' -g $RGV -v $RSV --backup-management-type AzureIaasVM --yes"
+        az backup container unregister -c "$CONTAINER1" -g $RGV -v $RSV --backup-management-type AzureIaasVM --yes
     }   
 
 Write-Host "-----------------------------------------------------"
