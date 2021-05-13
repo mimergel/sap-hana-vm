709fa1a8740dc72b7120c029866fb68b6982e906
Michael Mergell <michael.mergell@microsoft.com>
2021-05-13 14:23:05 +0200

    backup script simplification

--- Scripts/BackupDisable.ps1
+++ Scripts/BackupDisable.ps1
@@ -5,16 +5,14 @@
 	This script disables the OS & HANA Backups for a specific VM and HANA DB when relavant configuration can be found
 .EXAMPLE
     Test the script:
+    $SID="MM6"
     $RGV="HANABackups"
     $RSV="hanabackupvault"
     $VM="hanatest06"
     $VMRG="rg-HANA-MM6"
-    $POL="HANA-Non-PRD"
-    $ITEMSYS="saphanadatabase;mm6;systemdb"
-    $ITEMTEN="saphanadatabase;mm6;mm6"
-    $CONTAINER="VMAppContainer;Compute;$VMRG;$VM"
+    $POL="Non-PRD"
 
-    ./Scripts/BackupDisable.ps1 -RGV $RGV -RSV $RSV -VM $VM -VMRG $VMRG -POL $POL -ITEMSYS $ITEMSYS -ITEMTEN $ITEMTEN -CONTAINER $CONTAINER
+    ./Scripts/BackupDisable.ps1 -SID $SID -RGV $RGV -RSV $RSV -VM $VM -VMRG $VMRG -POL $POL
 
     some helpful commands:
     az backup protectable-item list -g HANABackups -v hanabackupvault --workload-type SAPHANA  --output table
@@ -28,20 +26,25 @@
 #>
 
 param(
+    [Parameter(Mandatory = $true)][string]$SID,     
     [Parameter(Mandatory = $true)][string]$RGV, 
     [Parameter(Mandatory = $true)][string]$RSV,
     [Parameter(Mandatory = $true)][string]$VM,
     [Parameter(Mandatory = $true)][string]$VMRG,
-    [Parameter(Mandatory = $true)][string]$POL,
-    [Parameter(Mandatory = $true)][string]$ITEMSYS,
-    [Parameter(Mandatory = $true)][string]$ITEMTEN,
-    [Parameter(Mandatory = $true)][string]$CONTAINER
+    [Parameter(Mandatory = $true)][string]$POL
 )
+
+$CONTAINER1="IaasVMContainer;iaasvmcontainerv2;$VMRG;$VM"
+$CONTAINER2="VMAppContainer;Compute;$VMRG;$VM"
+$ITEMSYS="saphanadatabase;$SID;systemdb"
+$ITEMTEN="saphanadatabase;$SID;$SID"
+$HANAPOL="HANA-$POL"
+$OSPOL="OS-$POL"
 
 Write-Host "-----------------------------------------------------"
 Write-Host "----------Disable existing HANA backup items---------"
-Write-Host "az backup protectable-item  list -c '$CONTAINER' -g $RGV -v $RSV --workload-type SAPHANA --output tsv"
-$PROTECT=az backup protectable-item  list -c "$CONTAINER" -g $RGV -v $RSV --workload-type SAPHANA --output tsv
+Write-Host "az backup protectable-item  list -c '$CONTAINER2' -g $RGV -v $RSV --workload-type SAPHANA --output tsv"
+$PROTECT=az backup protectable-item  list -c "$CONTAINER2" -g $RGV -v $RSV --workload-type SAPHANA --output tsv
 Write-Host $PROTECT
 
     if([string]::IsNullOrEmpty($PROTECT)){
@@ -49,26 +52,26 @@ Write-Host $PROTECT
     }
     else {
         Write-Host "---------Found items will be disabled----------------"
-        Write-Host "az backup protection disable -c '$CONTAINER' --delete-backup-data true --item-name '$ITEMSYS' -g $RGV -v $RSV --yes"
-        az backup protection disable -c "$CONTAINER" --delete-backup-data true --item-name "$ITEMSYS" -g $RGV -v $RSV --yes
-        Write-Host "az backup protection disable -c '$CONTAINER' --delete-backup-data true --item-name '$ITEMSTEN' -g $RGV -v $RSV --yes"
-        az backup protection disable -c "$CONTAINER" --delete-backup-data true --item-name "$ITEMTEN" -g $RGV -v $RSV --yes
+        Write-Host "az backup protection disable -c '$CONTAINER2' --delete-backup-data true --item-name '$ITEMSYS' -g $RGV -v $RSV --yes"
+        az backup protection disable -c "$CONTAINER2" --delete-backup-data true --item-name "$ITEMSYS" -g $RGV -v $RSV --yes
+        Write-Host "az backup protection disable -c '$CONTAINER2' --delete-backup-data true --item-name '$ITEMSTEN' -g $RGV -v $RSV --yes"
+        az backup protection disable -c "$CONTAINER2" --delete-backup-data true --item-name "$ITEMTEN" -g $RGV -v $RSV --yes
         Write-Host "-----------------------------------------------------"
         Write-Host ""
     }
 
 Write-Host "-----------------------------------------------------"
 Write-Host "----------------Unregister Container-----------------"
-$CONTDIS=az backup container show  -g $RGV -v $RSV --name "$CONTAINER"
-Write-Host $CONTDIS
+$CONTDIS2=az backup container show  -g $RGV -v $RSV --name "$CONTAINER2"
+Write-Host $CONTDIS2
 
-    if([string]::IsNullOrEmpty($CONTDIS)){
+    if([string]::IsNullOrEmpty($CONTDIS2)){
         Write-Host "-------------No Container for disabling--------------"
     }
     else {
         Write-Host "-------------Container will be disabled--------------"
-        Write-Host "az backup container unregister -c '$CONTAINER' -g $RGV -v $RSV --backup-management-type AzureWorkload --yes"
-        az backup container unregister -c "$CONTAINER" -g $RGV -v $RSV --backup-management-type AzureWorkload --yes
+        Write-Host "az backup container unregister -c '$CONTAINER2' -g $RGV -v $RSV --backup-management-type AzureWorkload --yes"
+        az backup container unregister -c "$CONTAINER2" -g $RGV -v $RSV --backup-management-type AzureWorkload --yes
     }   
 
 Write-Host "-----------------------------------------------------"
@@ -76,8 +79,8 @@ Write-Host ""
 
 Write-Host "-----------------------------------------------------"
 Write-Host "---------------Checking results----------------------"
-Write-Host "az backup protectable-item  list -c '$CONTAINER' -g $RGV -v $RSV --workload-type SAPHANA --output tsv"
-az backup protectable-item  list -c "$CONTAINER" -g $RGV -v $RSV --workload-type SAPHANA --output tsv
+Write-Host "az backup protectable-item  list -c '$CONTAINER2' -g $RGV -v $RSV --workload-type SAPHANA --output tsv"
+az backup protectable-item  list -c "$CONTAINER2" -g $RGV -v $RSV --workload-type SAPHANA --output tsv
 Write-Host "-----------------------------------------------------"
 Write-Host ""
 
@@ -85,17 +88,17 @@ Write-Host ""
 Write-Host "-----------------------------------------------------"
 Write-Host "----------------Delete IaaS Backups -----------------"
 
-$CONTAINER2="IaasVMContainer;iaasvmcontainerv2;$VMRG;$VM"
-$CONTDIS2=az backup container list -g $RGV -v $RSV --backup-management-type AzureIaasVM --query "[?name=='$CONTAINER2']"
-Write-Host $CONTDIS2
 
-    if([string]::IsNullOrEmpty($CONTDIS2)){
+$CONTDIS1=az backup container list -g $RGV -v $RSV --backup-management-type AzureIaasVM --query "[?name=='$CONTAINER1']"
+Write-Host $CONTDIS1
+
+    if([string]::IsNullOrEmpty($CONTDIS1)){
         Write-Host "-------------No IaaS Container found ----------------"
     }
     else {
         Write-Host "-------Container found, backup will be removed-------"
-        Write-Host "az backup protection disable -c "$CONTAINER2" -g $RGV -v $RSV --item-name $VM"
-        az backup protection disable -c "$CONTAINER2" -g $RGV -v $RSV --item-name $VM --yes
+        Write-Host "az backup protection disable -c "$CONTAINER1" -g $RGV -v $RSV --item-name $VM"
+        az backup protection disable -c "$CONTAINER1" -g $RGV -v $RSV --item-name $VM --yes
     }   
 
 Write-Host "-----------------------------------------------------"
@@ -103,7 +106,7 @@ Write-Host ""
 
 Write-Host "-----------------------------------------------------"
 Write-Host "---------------Checking results----------------------"
-Write-Host "az backup item show -c "$CONTAINER" -g $RGV -v $RSV --name $VM"
-az backup item show -c "$CONTAINER2" -g $RGV -v $RSV --name $VM
+Write-Host "az backup item show -c "$CONTAINER1" -g $RGV -v $RSV --name $VM"
+az backup item show -c "$CONTAINER1" -g $RGV -v $RSV --name $VM
 Write-Host "-----------------------------------------------------"
 Write-Host ""
--- Scripts/BackupEnable.ps1
+++ Scripts/BackupEnable.ps1
@@ -7,16 +7,14 @@
 	The script requires as prerequesite a successfully finished pre-registration script from here:
 .EXAMPLE
     Test the script:
+    $SID="MM6"
     $RGV="HANABackups"
     $RSV="hanabackupvault"
     $VM="hanatest06"
     $VMRG="rg-HANA-MM6"
-    $POL="HANA-Non-PRD"
-    $ITEMSYS="saphanadatabase;mm6;systemdb"
-    $ITEMTEN="saphanadatabase;mm6;mm6"
-    $CONTAINER="VMAppContainer;Compute;$VMRG;$VM"
+    $POL="Non-PRD"
 
-    ./Scripts/BackupEnable.ps1 -RGV $RGV -RSV $RSV -VM $VM -VMRG $VMRG -POL $POL -ITEMSYS $ITEMSYS -ITEMTEN $ITEMTEN -CONTAINER $CONTAINER
+    ./Scripts/BackupEnable.ps1 -SID $SID -RGV $RGV -RSV $RSV -VM $VM -VMRG $VMRG -POL $POL
 
     some helpful commands:
     az backup protectable-item list -g HANABackups -v hanabackupvault --workload-type SAPHANA  --output table
@@ -30,17 +28,20 @@
 #>
 
 param(
-    [Parameter(Mandatory = $true)][string]$RGV, 
+    [Parameter(Mandatory = $true)][string]$SID,    
+    [Parameter(Mandatory = $true)][string]$RGV,
     [Parameter(Mandatory = $true)][string]$RSV,
     [Parameter(Mandatory = $true)][string]$VM,
     [Parameter(Mandatory = $true)][string]$VMRG,
-    [Parameter(Mandatory = $true)][string]$POL,
-    [Parameter(Mandatory = $true)][string]$ITEMSYS,
-    [Parameter(Mandatory = $true)][string]$ITEMTEN,
-    [Parameter(Mandatory = $true)][string]$CONTAINER
+    [Parameter(Mandatory = $true)][string]$POL
 )
 
-$VMID = ""
+$CONTAINER1="IaasVMContainer;iaasvmcontainerv2;$VMRG;$VM"
+$CONTAINER2="VMAppContainer;Compute;$VMRG;$VM"
+$ITEMSYS="saphanadatabase;$SID;systemdb"
+$ITEMTEN="saphanadatabase;$SID;$SID"
+$HANAPOL="HANA-$POL"
+$OSPOL="OS-$POL"
 
 Write-Host "-----------------------------------------------------"
 Write-Host "-----------Get VM ID---------------------------------" 
@@ -50,34 +51,53 @@ Write-Host "$VMID" -ForegroundColor Red
 Write-Host "-----------------------------------------------------"
 Write-Host ""
 
+Write-Host "-----------------------------------------------------"
+Write-Host "-----------Check if VM is OS backup enabled----------"  
+Write-Host "az backup protection check-vm --vm $VMID" 
+$PROTECT1=az backup protection check-vm --vm $VMID
+Write-Host "-----------------------------------------------------"
+Write-Host ""
+
+    if ([string]::IsNullOrEmpty($PROTECT1)) {
+        Write-Host "--------VM Backup will be enabled----------------" 
+        Write-Host "az backup protection enable-for-vm -g $RGV -v $RSV --vm $VMID --policy-name $OSPOL"
+        az backup protection enable-for-vm -g $RGV -v $RSV --vm $VMID --policy-name $OSPOL
+        Write-Host "-----------------------------------------------------"
+    }
+    else {
+        Write-Host ""
+        Write-Host "--------Container is already in place----------------" 
+        Write-Host ""
+    }
+
 Write-Host "-----------------------------------------------------" 
 Write-Host "---------------List protectable items----------------" 
-Write-Host "az backup protectable-item  list -c '$CONTAINER' -g $RGV -v $RSV --workload-type SAPHANA --output tsv" 
-$PROTECT=az backup protectable-item  list -c "$CONTAINER" -g $RGV -v $RSV --workload-type SAPHANA --output tsv
-Write-Host $PROTECT
+Write-Host "az backup protectable-item  list -c '$CONTAINER2' -g $RGV -v $RSV --workload-type SAPHANA --output tsv" 
+$PROTECT2=az backup protectable-item  list -c "$CONTAINER2" -g $RGV -v $RSV --workload-type SAPHANA --output tsv
+Write-Host $PROTECT2
 Write-Host "-----------------------------------------------------" 
 Write-Host ""
 
 Write-Host "-----------------------------------------------------"
 Write-Host "-----Register the container if not yet in place -----" 
 
-    if ([string]::IsNullOrEmpty($PROTECT)) {
+    if ([string]::IsNullOrEmpty($PROTECT2)) {
         Write-Host "--------Container will be registered-----------------" 
         Write-Host "az backup container register -g $RGV -v $RSV --backup-management-type AzureWorkload --workload-type SAPHanaDatabase --resource-id $VMID" 
         az backup container register -g $RGV -v $RSV --backup-management-type AzureWorkload --workload-type SAPHanaDatabase --resource-id $VMID
         Write-Host "-----------------------------------------------------"
         Write-Host "-------------------Discovery-------------------------" 
-        Write-Host "az backup protectable-item initialize -g $RGV -v $RSV --workload-type SAPHanaDatabase -c '$CONTAINER'" 
-        az backup protectable-item initialize -g $RGV -v $RSV --workload-type SAPHanaDatabase -c "$CONTAINER"
+        Write-Host "az backup protectable-item initialize -g $RGV -v $RSV --workload-type SAPHanaDatabase -c '$CONTAINER2'" 
+        az backup protectable-item initialize -g $RGV -v $RSV --workload-type SAPHanaDatabase -c "$CONTAINER2"
         Write-Host "-----------------------------------------------------"
         Write-Host "-----------------------------------------------------"
         Write-Host "------------Enable SYSTEM DB Backups-----------------"  
-        Write-Host "az backup protection enable-for-azurewl -g $RGV -v $RSV --policy-name $POL --protectable-item-name '$ITEMSYS' --protectable-item-type SAPHANADatabase --server-name $VM --workload-type SAPHanaDatabase" 
-        az backup protection enable-for-azurewl -g $RGV -v $RSV --policy-name $POL --protectable-item-name "$ITEMSYS" --protectable-item-type SAPHANADatabase --server-name $VM --workload-type SAPHanaDatabase
+        Write-Host "az backup protection enable-for-azurewl -g $RGV -v $RSV --policy-name $HANAPOL --protectable-item-name '$ITEMSYS' --protectable-item-type SAPHANADatabase --server-name $VM --workload-type SAPHanaDatabase" 
+        az backup protection enable-for-azurewl -g $RGV -v $RSV --policy-name $HANAPOL --protectable-item-name "$ITEMSYS" --protectable-item-type SAPHANADatabase --server-name $VM --workload-type SAPHanaDatabase
         Write-Host ""
         Write-Host "------------Enable TENANT DB Backups-----------------"  
-        Write-Host "az backup protection enable-for-azurewl -g $RGV -v $RSV --policy-name $POL --protectable-item-name '$ITEMTEN' --protectable-item-type SAPHANADatabase --server-name $VM --workload-type SAPHanaDatabase" 
-        az backup protection enable-for-azurewl -g $RGV -v $RSV --policy-name $POL --protectable-item-name "$ITEMTEN" --protectable-item-type SAPHANADatabase --server-name $VM --workload-type SAPHanaDatabase
+        Write-Host "az backup protection enable-for-azurewl -g $RGV -v $RSV --policy-name $HANAPOL --protectable-item-name '$ITEMTEN' --protectable-item-type SAPHANADatabase --server-name $VM --workload-type SAPHanaDatabase" 
+        az backup protection enable-for-azurewl -g $RGV -v $RSV --policy-name $HANAPOL --protectable-item-name "$ITEMTEN" --protectable-item-type SAPHANADatabase --server-name $VM --workload-type SAPHanaDatabase
         Write-Host ""
     }
     else {
@@ -86,16 +106,13 @@ Write-Host "-----Register the container if not yet in place -----"
         Write-Host ""
     }
 
-Write-Host "-----------------------------------------------------"
-Write-Host "---------------List protectable items----------------"  
-Write-Host "az backup protectable-item  list -c '$CONTAINER' -g $RGV -v $RSV --workload-type SAPHanaDatabase --output tsv" 
-az backup protectable-item  list -c "$CONTAINER" -g $RGV -v $RSV --workload-type SAPHanaDatabase --output tsv
-Write-Host "-----------------------------------------------------"
-Write-Host ""
-
-Write-Host "Uncomment following lines to activate immediate initial HANA backups"
+Write-Host "Uncomment following lines to activate immediate initial OS & HANA backups"
 # Write-Host "-----------------------------------------------------"
-# Write-Host "-------------------Run Backups-----------------------" 
+# Write-Host "-------------------Run OS Backups------------------" 
+# az backup protection backup-now -g $RGV -v $RSV -c $CONTAINER1 --item-name $VM
+# Write-Host ""
+# Write-Host "-----------------------------------------------------"
+# Write-Host "-------------------Run HANA Backups------------------" 
 # Write-Host "az backup protection backup-now -g $RGV -v $RSV --item-name '$ITEMSYS' --container-name '$CONTAINER' --backup-type full" 
 # az backup protection backup-now -g $RGV -v $RSV --item-name "$ITEMSYS" --container-name "$CONTAINER" --backup-type full
 # Write-Host "az backup protection backup-now -g $RGV -v $RSV --item-name '$ITEMTEN' --container-name '$CONTAINER' --backup-type full" 
